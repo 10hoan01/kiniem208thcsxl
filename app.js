a0898ce75f3a73d6cbbafe0b6e588281edda9ece
10hoan01 <hoanhuu27022008@gmail.com>
2023-06-15 09:50:41 +0700

    upload source code

---
+++ app.js
@@ -0,0 +1,97 @@
+/*hoàn*/
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = '#29323c';
+  } else {
+    header.style.backgroundColor = 'transparent';
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+  });
+});
+
+let alerts = document.querySelectorAll('.alert');
+alerts.forEach(item=>{
+    item.addEventListener('click', function(event){
+        if(event.target.classList.contains('close')){
+            item.style.display = 'none';
+        }
+    })
+})
+window.addEventListener('offline', function(){
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'grid';
+})
+window.addEventListener('online', function(){
+    document.getElementById('error').style.display = 'none';
+    document.getElementById('success').style.display ='grid';
+});
+/*Effect Rời Trang*/
+const currenTitle = document.title;
+
+window.addEventListener("blur", () => {
+  document.title = "Bro bao h thì quay lại?";
+});
+window.addEventListener("focus", () => {
+  document.title = currenTitle;
+});
+/*End Effect Rời Trang*/
+/*Slider Img*/
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+
+    
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
+
+
+/*End Slider Img*/
+
